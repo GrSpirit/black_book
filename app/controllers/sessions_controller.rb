36a1652a972cae983a68232fb23707b684262512
GrSpirit <gr.spirit@gmail.com>
2019-01-27 22:04:32 +0400

    Users start

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  # Log in page
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authernticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == ?1 ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
