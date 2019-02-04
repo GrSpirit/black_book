6253a51adb4478d01ea66622f74c96581b04397f
GrSpirit <gr.spirit@gmail.com>
2019-02-04 21:34:00 +0400

    Coffe to js

---
+++ app/javascript/components/Deposits.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Deposits extends React.Component {
+  render() {
+    return (
+      <div className='deposits'>
+        <h2 className='title'>Deposits</h2>
+      </div>
+      )
+  }
+}
+
+export default Deposits
