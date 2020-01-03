e225ba871e625c32ca5da87d396921ab58572db3
GrSpirit <gr.spirit@gmail.com>
2020-01-03 21:59:43 +0400

    Move devise route to api

--- railsback/config/routes.rb
+++ railsback/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   scope :api, defaults: {format: :json} do
+    devise_for :users
     resources :deposits
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
