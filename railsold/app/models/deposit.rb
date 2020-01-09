a89d536deaa05c962b00e9179076b420f66df00a
GrSpirit <gr.spirit@gmail.com>
2020-01-09 15:48:17 +0400

    Removed old branch

--- railsold/app/models/deposit.rb
+++
@@ -1,7 +0,0 @@
-class Deposit < ApplicationRecord
-  validates :bank, presence: true, length: { maximum: 255 }
-  validates :date_start, presence: true
-  validates :date_end, presence: true
-  validates :amount, presence: true
-  validates :percent, presence: true
-end
